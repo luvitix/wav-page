f182f3c97be2fed3c1d11fe5edd6abbb39778351
luvitix <happymp4255@gmail.com>
2024-01-04 01:17:28 +0900

    waview

--- app.js
+++ app.js
@@ -1,42 +1,28 @@
-const express = require('express');
+
 const path = require('path');
 const fs = require('fs');
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
-const { StaticRouter } = require('react-router-dom');
-const App = require('./src/App'); // App.js에는 위에서 제공한 React Router 예제를 포함해야 합니다.
 
-const app = express();
+exports.handler = async (event) => {
+  const id1 = event.path.split('/').pop(); // 경로에서 id1 추출
+  const dynamicHtmlPath = path.join(__dirname, '..', 'views', 'server.html');
 
-app.use(express.static(path.join('public')));
+  try {
+    // server.html 파일 읽기
+    const data = fs.readFileSync(dynamicHtmlPath, 'utf8');
 
-app.get('/:id1', (req, res) => {
-    const id1 = req.params.id1;
-    const context = {};
+    // 동적 컨텐츠 교체
+    const dynamicHtml = data.replace(/{{objektId}}/g, id1);
 
-    // React 컴포넌트를 서버에서 렌더링
-    const appHtml = ReactDOMServer.renderToString(
-        <StaticRouter location={`/${id1}`} context={context}>
-            <App />
-        </StaticRouter>
-    );
-
-    // 서버에서 server.html 파일 읽기
-    const dynamicHtmlPath = path.join('views', 'server.html');
-    fs.readFile(dynamicHtmlPath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            // 읽어온 HTML 코드에 동적인 부분을 userId로 대체
-            const dynamicHtml = data.replace(/{{objektId}}/g, id1);
-            // React 애플리케이션의 렌더링 결과를 동적인 부분에 추가
-            const finalHtml = dynamicHtml.replace('<div id="app"></div>', `<div id="app">${appHtml}</div>`);
-            res.send(finalHtml);
-        }
-    });
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+    // 응답 반환
+    return {
+      statusCode: 200,
+      body: dynamicHtml,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error',
+    };
+  }
+};
