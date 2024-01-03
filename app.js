87a0846a84ca003803085b1aa6abffdca98e71ab
luvitix <happymp4255@gmail.com>
2024-01-04 01:14:08 +0900

    wav

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const { StaticRouter } = require('react-router-dom');
+const App = require('./src/App'); // App.js에는 위에서 제공한 React Router 예제를 포함해야 합니다.
+
 const app = express();
-// const port = 3000;
 
 app.use(express.static(path.join('public')));
 
-
 app.get('/:id1', (req, res) => {
     const id1 = req.params.id1;
-    const dynamicHtmlPath = path.join('views', 'server.html');
+    const context = {};
+
+    // React 컴포넌트를 서버에서 렌더링
+    const appHtml = ReactDOMServer.renderToString(
+        <StaticRouter location={`/${id1}`} context={context}>
+            <App />
+        </StaticRouter>
+    );
 
     // 서버에서 server.html 파일 읽기
+    const dynamicHtmlPath = path.join('views', 'server.html');
     fs.readFile(dynamicHtmlPath, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
@@ -19,25 +30,13 @@ app.get('/:id1', (req, res) => {
         } else {
             // 읽어온 HTML 코드에 동적인 부분을 userId로 대체
             const dynamicHtml = data.replace(/{{objektId}}/g, id1);
-            res.send(dynamicHtml);
+            // React 애플리케이션의 렌더링 결과를 동적인 부분에 추가
+            const finalHtml = dynamicHtml.replace('<div id="app"></div>', `<div id="app">${appHtml}</div>`);
+            res.send(finalHtml);
         }
     });
-    // fs.readFile(`${masterfile}/tx_${id2}.txt`, 'utf8', (err, data) => {
-    //     if (err) {
-    //         console.error(err);
-    //         // 파일 읽기 실패 시 에러 처리 로직을 추가하세요.
-    //     } else {
-    //         const fileContentLines = data.split('\n');
-    //         const firstLine = fileContentLines[0];
-    //         console.log(firstLine);
-    //         // 파일 읽기 성공 시 콘솔에 데이터 출력
-    //     }
-    // });
 });
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-// app.listen(port, () => {
-//     console.log(`Server is running at http://localhost:${port}`);
-// });
